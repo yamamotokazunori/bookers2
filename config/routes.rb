81a81bf0e55e782c94a06883752bd4a8104fd6ab
yamamotokazunori <gaogaigar673@gmail.com>
2021-04-10 04:54:48 +0000

    [add]ルーティング/TOPとaboutのレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   root to: 'homes#top'
-  resources :users, :only => [:index, :show, ]
-
+  resources :users, :only => [:index, :show, :edit, :update,]
+  resources :books
+  get "homes/about"
 
 end
